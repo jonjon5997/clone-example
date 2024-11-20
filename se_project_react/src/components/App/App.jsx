024fa185608da30b9159d4bf2687c886b1677322
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-11-20 18:25:41 -0500

    update weather card

--- se_project_react/src/components/App/App.jsx
+++ se_project_react/src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
+import WeatherCard from "../WeatherCard/WeatherCard";
 
 function App() {
   return (
     <div className="page">
       <div className="page__content">
         <Header />
+        <Main />
       </div>
     </div>
   );
---
+++ se_project_react/src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+
+        {/* {todo - add the cards} */}
+      </section>
+    </main>
+  );
+}
+
+export default Main;
