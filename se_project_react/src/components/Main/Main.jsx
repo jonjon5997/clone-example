e7b99316e02c63b51746639eef0b7f586a7756b5
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-11-21 17:37:00 -0500

    render item cards

--- se_project_react/src/components/Main/Main.jsx
+++ se_project_react/src/components/Main/Main.jsx
@@ -1,4 +1,6 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
 
 function Main() {
   return (
@@ -8,7 +10,11 @@ function Main() {
         <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
-
+        <ul className="cards__list">
+          {defaultClothingItems.map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}
+        </ul>
         {/* {todo - add the cards} */}
       </section>
     </main>
