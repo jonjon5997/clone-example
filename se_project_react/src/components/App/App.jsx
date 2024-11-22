85fa026c6c33fcf037aa11d4a3d5211965dc3460
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-11-22 11:31:44 -0500

    modal form refactor

--- se_project_react/src/components/App/App.jsx
+++ se_project_react/src/components/App/App.jsx
@@ -1,15 +1,59 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import WeatherCard from "../WeatherCard/WeatherCard";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" }); //left part of object is variable name and the second part is the function you can use to change the variable -> variableName, setVariableName
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />{" "}
+        {/* pass the variable to the main componenet as a prop*/}
       </div>
+      <ModalWithForm title="New Garment" buttonText="Add Garment">
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            placeholder="name"
+            type="text"
+            className="modal__input"
+            id="name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            placeholder="imageUrl"
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          {" "}
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+          </label>{" "}
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
--- se_project_react/src/components/Main/Main.jsx
+++ se_project_react/src/components/Main/Main.jsx
@@ -1,8 +1,9 @@
+import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
@@ -11,9 +12,14 @@ function Main() {
           Today is 75 &deg; F / You may want to wear:
         </p>
         <ul className="cards__list">
-          {defaultClothingItems.map((item) => {
-            return <ItemCard key={item._id} item={item} />;
-          })}
+          {defaultClothingItems
+            .filter((item) => {
+              //returns a boolean
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
         </ul>
         {/* {todo - add the cards} */}
       </section>
